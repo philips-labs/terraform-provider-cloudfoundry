9fd0466bbfced1cf4cba589e2d0dbc2bb3f968a6
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-03-12 06:37:58 +0100

    Support for local debugging

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/terraform-providers/terraform-provider-cloudfoundry/cloudfoundry"
+	"log"
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: cloudfoundry.Provider})
+	var debugMode bool
+
+	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	opts := &plugin.ServeOpts{
+		ProviderFunc: cloudfoundry.Provider}
+
+	if debugMode {
+		err := plugin.Debug(context.Background(), "registry.terraform.io/philips-labs/cloudfoundry", opts)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		return
+	}
+	plugin.Serve(opts)
 
 }
